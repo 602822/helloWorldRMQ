0f335866b5d5890cc02cc1a05158ab56e7accb6c
602822 <89077126+602822@users.noreply.github.com>
2024-10-02 18:29:55 +0200

    done

---
+++ src/main/java/org/example/Worker.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Worker {
+
+    private static final String TASK_QUEUE_NAME = "task";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        int prefetchcount = 1;
+        channel.basicQos(prefetchcount);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        boolean autoAck = false;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {});
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for(char ch : task.toCharArray()) {
+            if(ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+}
